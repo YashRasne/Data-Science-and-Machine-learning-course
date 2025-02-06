a7b78d5ebffacc4aa7b9f73e05f8a15e57dae110
YashRasne <165058949+YashRasne@users.noreply.github.com>
2025-02-05 20:23:53 -0800

    Even odd number

--- day1.py
+++ day1.py
@@ -6,3 +6,12 @@ b = 20
 
 add = a+b
 print("Addition is",add)
+
+
+#this is second code even code
+num = int(input("Enter a number :"))
+if num%2==0:
+    print("Even number")
+
+else:
+    print("odd number")
