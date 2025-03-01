15ec66cf929e5787ea9066f9da71230cf063aabe
YashRasne <165058949+YashRasne@users.noreply.github.com>
2025-02-28 22:55:57 -0800

    calculator simple code

--- cal.py
+++ cal.py
@@ -14,7 +14,8 @@ def multiply(num1, num2):
 
 # Function to divide two numbers
 def divide(num1, num2):
-    return num1 / num2
+    return num1 / num2cls
+    
 
 print("Please select operation -\n" \
         "1. Add\n" \
